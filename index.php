72d4ecafa9a36fe7676a858fa7e6e92c6f160ac6
MIGOdanis <j0sdarg06@gmail.com>
2014-02-27 15:03:45 +0800

    1.2
    增加S-LINE

--- index.php
+++ index.php
@@ -1,17 +1,68 @@
 <?php
 if(!isset($_POST['UFS'])){
 ?>
+<style type="text/css">
+	body{
+		font-family: "微軟正黑體";
+	}
+	.s-line-group, .ufs{
+		width: 1280px;
+	}
+
+
+</style>
 <meta charset='utf-8'>
+<div id="main">
+<h1>CG 繪圖機</h1>
 <form action="" method="post">
-　UNMODIFIED FORWARD SEQUENCE<br>
-　<textarea name="UFS" cols="150" rows="40"></textarea><br>
-　<input type="submit" value="輸出">
+	<div class="ufs">
+	 	<fieldset>
+	    	<legend>UNMODIFIED FORWARD SEQUENCE</legend>
+			<textarea name="UFS" cols="150" rows="15"></textarea>
+		</fieldset>
+	</div>
+	<div class="s-line-group">
+	 	<fieldset>
+	    	<legend>S-Line - 左側欄位輸入敘述，右側欄位輸入字串</legend>
+			<div class="s-line">
+				1.<input type="text" name="s-text-1" value=""  />
+				<input type="text" name="s-line-1" size="170" value="" />
+			</div>
+			<div class="s-line">
+				2.<input type="text" name="s-text-2" value=""  />
+				<input type="text" name="s-line-2" size="170" value="" />
+			</div>
+			<div class="s-line">
+				3.<input type="text" name="s-text-3" value=""  />
+				<input type="text" name="s-line-3" size="170" value="" />
+			</div>
+			<div class="s-line">
+				4.<input type="text" name="s-text-4" value=""  />
+				<input type="text" name="s-line-4" size="170" value="" />
+			</div>
+			<div class="s-line">
+				5.<input type="text" name="s-text-5" value=""  />
+				<input type="text" name="s-line-5" size="170" value="" />
+			</div>
+			<div class="s-line">
+				6.<input type="text" name="s-text-6" value=""  />
+				<input type="text" name="s-line-6" size="170" value="" />
+			</div>
+			<div class="s-line">
+				7.<input type="text" name="s-text-7" value=""  />
+				<input type="text" name="s-line-7" size="170" value="" />
+			</div>
+		</fieldset>
+	</div>
+<input type="submit" name="submit" value="繪製">
 </form>
+</div>
 <?php
 }else{
 	//尺規總長
 	$length = mb_strlen($_POST['UFS'], 'utf-8');
 	$cgLineY = 300;
+	$sLineY = 120;
 	$footY = 100;
 	$font = "arial.ttf";
 	//把cg加上逗號
@@ -132,6 +183,40 @@ if(!isset($_POST['UFS'])){
 		imageline($im, 20, $cgLineY+1, $dex, $cgLineY+1, $color);
 		return array($im,$dex);
 	}
+
+	function SLine($im, $ll, $r){
+		global $font,$length,$sLineY;
+		$sx = 20;
+		$partNum = ($length/100);
+		$partPx = (($ll-20)/$partNum)/100;
+
+		$color[1] = imagecolorallocate($im, 27, 166, 194);
+		$color[2] = imagecolorallocate($im, 27, 194, 33);
+		$color[3] = imagecolorallocate($im, 235, 227, 16);
+		$color[4] = imagecolorallocate($im, 234, 16, 103);
+		$color[5] = imagecolorallocate($im, 234, 118, 16);
+		$color[6] = imagecolorallocate($im, 102, 80, 60);
+		$color[7] = imagecolorallocate($im, 0, 70, 105);
+		$black = imagecolorallocate($im, 0, 0, 0);
+
+		$slength = mb_strlen($_POST['s-line-'.$r], 'utf-8');
+		$startPx = strpos($_POST['UFS'],$_POST['s-line-'.$r],0);
+		$sx = $partPx * $startPx;
+		$ex = $sx + ($slength * $partPx);
+
+		imageline($im, $sx, $sLineY, $ex, $sLineY, $color[$r]);
+		imageline($im, $sx, $sLineY+1, $ex, $sLineY+1, $color[$r]);
+		imageline($im, $sx, $sLineY+2, $ex, $sLineY+2, $color[$r]);
+		imageline($im, $sx, $sLineY+3, $ex, $sLineY+3, $color[$r]);
+		imageline($im, $sx, $sLineY+4, $ex, $sLineY+4, $color[$r]);
+
+		if(!empty($_POST['s-text-'.$r])){
+			$textX = $sx + (($ex - $sx)/2);
+			imagettftext($im, 15, 0, $textX-8, $sLineY+30, $black, $font, $_POST['s-text-'.$r]);
+		}
+		return $im;
+	}
+
 	header("Content-type: image/png");
 	//產生圖像
 	$im = imagecreate(860,600); //860 600
@@ -148,6 +233,14 @@ if(!isset($_POST['UFS'])){
 	$ima = rulers($im, $black);
 	$im = $ima[0];
 	$im = CGLine($im, $black, $ima[1]);
+
+	//畫s
+	for($r=1;$r<=7;$r++){
+		if(!empty($_POST['s-line-'.$r])){
+			$im = SLine($im, $ima[1], $r);
+		}
+	}
+
 	imagepng($im);
 	imagedestroy($im);
 }
